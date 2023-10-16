537e375a73bdadcef816ecbd1f025595588b7191
Xaver106 <xaver10610@gmail.com>
2023-10-16 11:45:29 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Ryan's NixOS Flake";
+
+  inputs = {
+
+    # Official NixOS package sources
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: rec {
+
+    nixosConfigurations = {
+      "Xavers-nixDesktop" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./shared
+          ./hosts/Xavers-nixDesktop/default.nix
+        ];
+      };
+    };
+  };
+}
---
+++ shared/default.nix
@@ -0,0 +1,190 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ lib, config, options, pkgs, modulesPath, ... }:
+
+{
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable Networking
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.xaver106 = {
+    isNormalUser = true;
+    description = "Xaver106";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+
+    # Unsorted
+    ansible
+    glances
+    kate
+    ranger
+    gparted
+    neofetch
+    filezilla
+    cryptomator
+    prusa-slicer
+    nextcloud-client
+    firefox
+    
+    thunderbird
+    betterbird
+    
+    # Authentication
+    yubioath-flutter
+    yubikey-manager
+    yubikey-manager-qt
+    yubikey-touch-detector
+    yubikey-personalization
+    yubikey-personalization-gui
+
+    # Development Tools
+    git
+    smartgithg
+
+    # Build Tools
+    maven
+    gradle
+
+    # IDEs
+    vscode
+    jetbrains-toolbox
+
+    # Latex
+    texlive.combined.scheme-full
+    jabref
+
+    # Man Pages + tldr
+    man-pages
+    man-pages-posix
+    tldr
+
+    # LibreOffice + Spell Checking
+    libreoffice-qt
+    hunspell
+    hunspellDicts.de_DE
+    hunspellDicts.en_US
+
+    # Minecraft
+    prismlauncher-qt5
+
+    # Communication
+    teamspeak_client
+    (pkgs.discord.override {
+      withOpenASAR = false;
+      withVencord = true;
+    })
+  ];
+
+
+  programs = {
+    fish.enable = true;
+    kdeconnect.enable = true;
+    _1password.enable = true;
+    _1password-gui = {
+      enable = true;
+      # Certain features, including CLI integration and system authentication support,
+      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+      polkitPolicyOwners = [ "xaver106" ];
+    };
+    ausweisapp = {
+      # Install German AusweisApp2 and open Firewall, so my phone can act ass a card reader
+      enable = true;
+      openFirewall = true;
+    };
+    java = {
+      enable = true;
+      package = pkgs.jdk17;
+    };
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
+  services = {
+    flatpak.enable = true;
+    pcscd.enable = true; # Needed for yubico authenticator
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
