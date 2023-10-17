ddc5dfff689e94c09092edc4efacbf2eed638729
Xaver106 <xaver10610@gmail.com>
2023-10-18 00:32:45 +0200

    improve home-manager import

--- flake.nix
+++ flake.nix
@@ -7,17 +7,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
+    # nixos-hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     
-    # home-manager, used for managing user configuration
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: rec {
