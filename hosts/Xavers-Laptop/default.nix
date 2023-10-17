80985ba3c725936180e1c16c658c7857eb158147
Xaver106 <xaver10610@gmail.com>
2023-10-17 19:03:50 +0200

    Added Laptop

--- hosts/Xavers-Laptop/default.nix
+++ hosts/Xavers-Laptop/default.nix
@@ -12,6 +12,10 @@
 
   networking.hostName = "Xavers-Laptop";
 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
