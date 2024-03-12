f4fb3c9ead8b827041b62c72eccb919b99860480
Xaver106 <xaver10610@gmail.com>
2024-03-12 10:39:22 +0100

    Change  Steam Deck software to Deckmaster

--- hosts/Xavers-nixDesktop/default.nix
+++ hosts/Xavers-nixDesktop/default.nix
@@ -21,12 +21,26 @@
 
   services.xserver.displayManager.defaultSession = "plasmax11";
 
+  environment.systemPackages = with pkgs; [
+    deckmaster # Elgato Stream Deck software
+  ];
+
+  services.udev.packages = [ (pkgs.writeTextDir "etc/udev/rules.d/99-streamdeck.rules"
+    ''
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0080", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-mini"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-xl"
+    ''
+    ) ];
+
   services.boinc.enable = true; # BOINC Client and Manager (Distributet computing service)
 
   # Streamdeck UI to control Elgato Stream Deck
   programs.streamdeck-ui = {
     enable = true;
-    autoStart = true; # optional
+    autoStart = false; # optional
   };
 
   # This value determines the NixOS release from which the default
