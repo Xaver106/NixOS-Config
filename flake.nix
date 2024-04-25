7bb1361188d797534a5989807c05ae73f42aabb9
Xaver106 <xaver10610@gmail.com>
2024-04-25 23:53:10 +0200

    init home-manager

--- flake.nix
+++ flake.nix
@@ -7,41 +7,58 @@
 
     # Official NixOS package sources
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
 
     # nixos-hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, nixos-hardware, ... }@inputs: rec {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: let
+    hm-defaults = {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+    };
+  in rec {
 
     nixosConfigurations = {
       "Xavers-nixDesktop" = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
-
-        specialArgs = {
-          nixpkgs-stable = import nixpkgs-stable {inherit system; config.allowUnfree = true;};
-        };
 
         modules = [
           ./shared
           ./hosts/Xavers-nixDesktop
           nixos-hardware.nixosModules.common-cpu-intel
           nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.xaver106 = import ./home/xaver106;
+          }
         ];
       };
       "Xavers-Laptop" = nixpkgs.lib.nixosSystem  rec {
         system = "x86_64-linux";
 
-        specialArgs = {
-          nixpkgs-stable = import nixpkgs-stable {inherit system; config.allowUnfree = true;};
-        };
-
         modules = [
           ./shared
           ./hosts/Xavers-Laptop
           nixos-hardware.nixosModules.framework-11th-gen-intel
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.xaver106 = import ./home/xaver106;
+          }
         ];
       };
     };
--- shared/default.nix
+++ shared/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, config, options, pkgs, modulesPath, nixpkgs-stable, ... }:
+{ lib, config, options, pkgs, ... }:
 
 {
 
