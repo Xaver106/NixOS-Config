5dd8031794bf6992f3579d987ed2fbc07dfa82d8
Xaver106 <xaver10610@gmail.com>
2024-04-26 11:53:26 +0200

    add ranger plugins

--- home/xaver106/default.nix
+++ home/xaver106/default.nix
@@ -4,7 +4,7 @@
   home.username = "xaver106";
   home.homeDirectory = "/home/xaver106";
 
-
+  programs.fish.enable = true;
 
   programs.zoxide.enable = true;
 
@@ -18,11 +18,36 @@
 
   programs.eza = {
     enable = true;
+    icons = true;
     extraOptions = [ "--group-directories-first" ];
   };
   
-  # TODO: Add Plugins
-  programs.ranger.enable = true;
+  programs.ranger = {
+    enable = true;
+    plugins = [
+      {
+        name = "ranger_devicons";
+        src = builtins.fetchGit {
+          url = "https://github.com/alexanderjeurissen/ranger_devicons.git";
+          rev = "a8d626485ca83719e1d8d5e32289cd96a097c861";
+        };
+      }
+      {
+        name = "zoxide";
+        src = builtins.fetchGit {
+          url = "https://github.com/jchook/ranger-zoxide.git";
+          rev = "281828de060299f73fe0b02fcabf4f2f2bd78ab3";
+        };
+      }
+    ];
+    settings = {
+      preview_images = "true";
+      preview_images_method = "kitty";
+    };
+    extraConfig = ''
+      default_linemode devicons
+    '';
+  };
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
