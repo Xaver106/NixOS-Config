4e264f7edc01346afba84cd0c74d6b344104155d
Xaver106 <xaver10610@gmail.com>
2024-06-23 23:36:35 +0200

    refactor: cleanup and update
    https://github.com/NixOS/nixpkgs/pull/320228

--- hosts/Xavers-nixDesktop/nvidia.nix
+++ hosts/Xavers-nixDesktop/nvidia.nix
@@ -1,15 +1,11 @@
 { config, lib, pkgs, ... }:
 {
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+  
+  # Enable graphics driver in NixOS
+  hardware.graphics.enable = true;
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
 
