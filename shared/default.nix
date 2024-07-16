02a894e922504628a012c7262ce615fe0f9405aa
Xaver106 <xaver10610@gmail.com>
2024-07-16 10:33:27 +0200

    refactor: sound update
    https://github.com/NixOS/nixpkgs/pull/326262

--- shared/default.nix
+++ shared/default.nix
@@ -88,8 +88,6 @@
   };
 
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
