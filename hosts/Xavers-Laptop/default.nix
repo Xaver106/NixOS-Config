bccc6a32bf0a3a84b07c905ea7d6d2d033f268af
Xaver106 <xaver10610@gmail.com>
2024-11-17 15:37:38 +0100

    add HibernateDelay to sleep conf

--- hosts/Xavers-Laptop/default.nix
+++ hosts/Xavers-Laptop/default.nix
@@ -24,6 +24,11 @@
 
   security.pam.services.login.fprintAuth = false; # Disable fingerprint login (Kwallet doesn't unlock with it)
 
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30min
+  '';
+
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
