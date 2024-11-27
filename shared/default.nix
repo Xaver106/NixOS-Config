e932a70e37e8e5f75ac168b45284e09187e220f9
Xaver106 <xaver10610@gmail.com>
2024-11-27 11:17:08 +0100

    add extra keyboard layouts

--- shared/default.nix
+++ shared/default.nix
@@ -13,6 +13,7 @@
       ./bluetooth.nix
       ./documentation.nix
       ./fonts.nix
+      ./layouts.nix
       ./locale-defaults.nix
       ./networking.nix
       ./plasma.nix
@@ -28,6 +29,7 @@
     bluetooth.enable = mkDefault true;
     documentation.enable = mkDefault true;
     fonts.enable = mkDefault true;
+    layouts.enable = mkDefault true;
     locale-defaults.enable = mkDefault true;
     networking.enable = mkDefault true;
     pkgs.enable = mkDefault true;
