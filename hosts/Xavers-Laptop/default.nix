be91a937e22afd8dbaf81219b374c7d55f0b219e
Xaver106 <xaver10610@gmail.com>
2024-12-01 21:40:54 +0100

    add AI and fix fonts

--- hosts/Xavers-Laptop/default.nix
+++ hosts/Xavers-Laptop/default.nix
@@ -28,6 +28,8 @@
     HibernateDelaySec=1h
   '';
 
+  shared.ai.enable = false;
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
