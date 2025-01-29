6de9645fb074f998b035cb1705f5a1c75b8a4ca0
Xaver106 <xaver106@posteo.de>
2025-01-29 01:40:24 +0100

    Desktop: steam deck software

--- hosts/Xavers-nixDesktop/default.nix
+++ hosts/Xavers-nixDesktop/default.nix
@@ -28,19 +28,7 @@
 
   time.hardwareClockInLocalTime = true; # Fix windows showing wrong time
 
-  environment.systemPackages = with pkgs; [
-    deckmaster # Elgato Stream Deck software
-  ];
-
-  services.udev.packages = [
-    (pkgs.writeTextDir "etc/udev/rules.d/99-streamdeck.rules" ''
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0080", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-mini"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", MODE:="666", GROUP="plugdev", SYMLINK+="streamdeck-xl"
-    '')
-  ];
+  programs.streamcontroller.enable = true;
 
   services.boinc.enable = true; # BOINC Client and Manager (Distributet computing service)
 
