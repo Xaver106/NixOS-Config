a4a9fccafd24f90185d9b6c218b546e2d3c0dafe
Xaver106 <xaver106@posteo.de>
2025-03-24 15:03:13 +0100

    add: waydroid virtualiation

--- hosts/common/optional/virtualisation.nix
+++ hosts/common/optional/virtualisation.nix
@@ -15,7 +15,10 @@ in {
       quickemu
     ];
 
-    virtualisation.libvirtd.enable = true;
+    virtualisation = {
+      libvirtd.enable = true;
+      waydroid.enable = true;
+    };
 
     # if you use libvirtd on a desktop environment
     programs.virt-manager.enable = true; # can be used to manage non-local hosts as well
